264c576f2cedf4b0c45a824c1811e14acc28cf07
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2023-06-14 21:32:40 +0200

    Remove NugetDependencies

--- src/Tester/BhoHandler.cs
+++ src/Tester/BhoHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediaSharp.Core;
 using MediaSharp.Core.Attributes;
+using Tester.Foooooooo.Qualcosa;
 using Tester.Request.Cristo;
 
 namespace Tester;
@@ -7,6 +8,8 @@ namespace Tester;
 [CallableHandler]
 public partial class BhoHandler : IRequestHandler<Bho, Qualcosa>
 {
+    private readonly IFoo foo;
+
     public async Task<Qualcosa> HandleAsync(Bho request, CancellationToken cancellationToken)
     {
         return await Task.Run(() => new Qualcosa() { Id = 1 });
